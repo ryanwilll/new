97852ef26b749b64947851e7e40191ecbec0f9e6
Ryan Will <ryan_will-@hotmail.com>
2023-01-24 23:16:15 -0300

    Iniciado JS e alterações no CSS resposivo

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const btnTrailerOpen = document.querySelector("#botao-trailer-open")
+const btnTrailerClose = document.querySelector("#botao-trailer-close")
+const modal = document.querySelector(".modal")
+
+
+btnTrailerOpen.addEventListener("click", (e) => {
+    modal.classList.add("aberto")
+
+})
+
+btnTrailerClose.addEventListener("click", (eClose) => {
+    modal.classList.remove("aberto")
+    stop()
+
+})
+
+function stop(){
+    var iframe = document.getElementById('video');
+    iframe.src = iframe.src;
+}
